62d6f26f90b2ba64587a172c0dd6f54a26bedc0b
Meg <meganriley806@gmail.com>
2020-08-31 10:10:50 -0500

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,15 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    class Meta:
+        ordering = ['-pub_date',]
 
-#Add to the admin
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_modified(self):
+        return self.pub_date.strftime('%b %e %Y')
